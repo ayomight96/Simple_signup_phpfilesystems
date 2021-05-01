3f3021460d374d8c52ff45dc8dd9c9839b992657
ayomight96 <33792487+ayomight96@users.noreply.github.com>
2021-05-01 14:24:59 +0100

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -3,8 +3,11 @@
     include 'functions.php';
     //converst all of user inputs into an array and assigns it to $user
     $user = make_array_signup();
+    //print_r($user);
+    print "<br>";
+    //print_r($_POST);
     //here the password array element is crosschecked with the password actually entered by the user
-    if ($_POST['password1'] == $user[4]){
+    if ($_POST['password1'] == $_POST['password']){
         print "Welcome to the Homepage, $user[1]";
         print "<br>";
         print "<br>";
@@ -16,6 +19,10 @@
         print "<tr><td>Your First Name:</td><td>$user[1]</td></tr>";
         print "<tr><td>Your Last Name:</td><td>$user[2]</td></tr>";
         print "<tr><td>Your Email:</td><td>$user[3]</td></tr>";
+        print "<tr><td></td></tr>";
+        print "<tr><td></td></tr>";
+        print "<tr><td><a href=\"reset.html\">Reset Password</a></td></tr>";
+
         print "</table>\n";
         print "<br>";
         print "<br>";
@@ -30,17 +37,11 @@
             print "Your registration was not successful!";
         }
 
+    } else {
+        print "Your password does not match<br>";
+        print "<a href=\"signup.html\">kindly click here to return to the sign up page </a>";
+
     }
  
 //Here a link is created to redirect user to page where password can be changed
 ?>
-<html>
-    <body>
-    <table> 
-    <form>
-            <tr><td><a href="reset.html">Reset Password</a></td></tr>        
-    </form>
-    </table>
-    </body>
-    </html>
- 
